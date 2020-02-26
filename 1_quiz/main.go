6b1c1d1e154b86377d37ac110005aa1b87a1c9c5
Lin Trieu <linnatrieu@gmail.com>
2020-02-26 20:44:36 +0000

    Completes part 1

--- 1_quiz/main.go
+++ 1_quiz/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -42,7 +43,7 @@ func parseLines(lines [][]string) []problem {
 	for i, line := range lines {
 		ret[i] = problem {
 			q: line[0],
-			a: line[1],
+			a: strings.TrimSpace(line[1]),
 		}
 	}
 	return ret
